f6a1623630afce59cd002b9ad0eff22e8a89eb10
Jacob Kinchen <jmkinchenjr@icloud.com>
2025-04-30 18:08:13 -0500

    Bug Fix with Enemy Speed

--- 4444_final_project/scripts/level.gd
+++ 4444_final_project/scripts/level.gd
@@ -4,16 +4,11 @@ extends Node2D   # or whichever root type you’re using
 @onready var time_label : Label = $Label
 
 func _ready():
-	# Connect the timer’s timeout signal
 	timer.timeout.connect(_on_LevelTimer_timeout)
-	# If you didn’t enable Autostart, you can start it here:
-	# timer.start()
 	
-	# Initialize display right away
 	_update_time_label(timer.time_left)
 
 func _process(delta):
-	# Each frame, update the on‐screen time left
 	_update_time_label(timer.time_left)
 
 func _update_time_label(time_left_sec: float) -> void:
@@ -26,6 +21,4 @@ func _update_time_label(time_left_sec: float) -> void:
 	time_label.text = "%02d:%03d" % [seconds, milliseconds]
 
 func _on_LevelTimer_timeout() -> void:
-	# Called when countdown reaches zero
-	# Change to your End Screen scene:
 	get_tree().change_scene_to_file("res://scenes/endscreen.tscn")
